773acf6e0202e3de4bc928657376640c187821c3
Vangelis <phungi@nenneke.space.warwick.ac.uk>
2017-10-27 13:23:37 +0100

    function

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -6,6 +6,53 @@
 // For std::isalpha and std::isupper
 #include <cctype>
 
+std:: string transformChar (const char in_Char)
+{
+    std::string inputText {""};
+
+    // Uppercase alphabetic characters
+    if (std::isalpha(in_Char)) {
+      inputText += std::toupper(in_Char);
+    }
+
+    // Transliterate digits to English words
+    switch (in_Char) {
+      case '0':
+        inputText += "ZERO";
+        break;
+      case '1':
+        inputText += "ONE";
+        break;
+      case '2':
+        inputText += "TWO";
+        break;
+      case '3':
+        inputText += "THREE";
+        break;
+      case '4':
+        inputText += "FOUR";
+        break;
+      case '5':
+        inputText += "FIVE";
+        break;
+      case '6':
+        inputText += "SIX";
+        break;
+      case '7':
+        inputText += "SEVEN";
+        break;
+      case '8':
+        inputText += "EIGHT";
+        break;
+      case '9':
+        inputText += "NINE";
+        break;
+    }
+    return inputText;
+
+    // If the character isn't alphabetic or numeric, DONT add it.
+    // Our ciphers can only operate on alphabetic characters.
+}
 // Main function of the mpags-cipher program
 int main(int argc, char* argv[])
 {
@@ -110,47 +157,7 @@ int main(int argc, char* argv[])
   // (until Return then CTRL-D (EOF) pressed)
   while(std::cin >> inputChar)
   {
-    // Uppercase alphabetic characters
-    if (std::isalpha(inputChar)) {
-      inputText += std::toupper(inputChar);
-    }
-
-    // Transliterate digits to English words
-    switch (inputChar) {
-      case '0':
-	inputText += "ZERO";
-	break;
-      case '1':
-	inputText += "ONE";
-	break;
-      case '2':
-	inputText += "TWO";
-	break;
-      case '3':
-	inputText += "THREE";
-	break;
-      case '4':
-	inputText += "FOUR";
-	break;
-      case '5':
-	inputText += "FIVE";
-	break;
-      case '6':
-	inputText += "SIX";
-	break;
-      case '7':
-	inputText += "SEVEN";
-	break;
-      case '8':
-	inputText += "EIGHT";
-	break;
-      case '9':
-	inputText += "NINE";
-	break;
-    }
-
-    // If the character isn't alphabetic or numeric, DONT add it.
-    // Our ciphers can only operate on alphabetic characters.
+      inputText+=transformChar(inputChar);
   }
 
   // Output the transliterated text
